88a8ec2894a5b5daa8e453bb9cdd492dc6551e24
Stanila Andrei <andrei133303@gmail.com>
2019-12-14 20:46:34 +0200

    Added missing annotations and verification for admin

--- src/main/java/com/code4ro/legalconsultation/controller/AssignmentController.java
+++ src/main/java/com/code4ro/legalconsultation/controller/AssignmentController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,17 +32,20 @@ public class AssignmentController {
         return new ResponseEntity<>(assignmentService.getAll(), HttpStatus.OK);
     }
 
+    @PostMapping(value = "/assign")
     public ResponseEntity<Assignment> save(@RequestBody final Assignment assignment) {
 
         return new ResponseEntity<>(assignmentService.save(assignment), HttpStatus.OK);
     }
 
+    @PutMapping(value = "/approve")
     public ResponseEntity<Void> approve(@PathVariable final UUID assignmentId){
         Assignment assignment = assignmentService.approve(assignmentId);
         if(assignment == null)return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @PutMapping(value = "/revoke")
     public ResponseEntity<Void> revoke(@PathVariable final UUID assignmentId){
         Assignment assignment = assignmentService.revoke(assignmentId);
         if(assignment == null)return new ResponseEntity<>(HttpStatus.NOT_FOUND);
--- src/main/java/com/code4ro/legalconsultation/service/impl/AssignmentService.java
+++ src/main/java/com/code4ro/legalconsultation/service/impl/AssignmentService.java
@@ -1,9 +1,14 @@
 package com.code4ro.legalconsultation.service.impl;
 
+import com.code4ro.legalconsultation.common.exceptions.LegalValidationException;
+import com.code4ro.legalconsultation.config.security.CurrentUserService;
+import com.code4ro.legalconsultation.model.persistence.ApplicationUser;
 import com.code4ro.legalconsultation.model.persistence.AssigmentStatus;
 import com.code4ro.legalconsultation.model.persistence.Assignment;
+import com.code4ro.legalconsultation.model.persistence.UserRole;
 import com.code4ro.legalconsultation.repository.AssignmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,10 +18,14 @@ import java.util.UUID;
 @Service
 public class AssignmentService {
 
+    private final CurrentUserService currentUserService;
+
     private final AssignmentRepository assignmentRepository;
 
     @Autowired
-    public AssignmentService(AssignmentRepository assignmentRepository) {
+    public AssignmentService(CurrentUserService currentUserService,
+            AssignmentRepository assignmentRepository) {
+        this.currentUserService = currentUserService;
         this.assignmentRepository = assignmentRepository;
     }
 
@@ -29,6 +38,10 @@ public class AssignmentService {
     }
 
     public Assignment approve(final UUID assigmentId) {
+        final ApplicationUser currentUser = currentUserService.getCurrentUser();
+        if(currentUser == null || currentUser.getUser().getRole() != UserRole.ADMIN)
+            throw new throw new LegalValidationException("comment.Unauthorized.user", HttpStatus.BAD_REQUEST);
+
         Optional<Assignment> optional = assignmentRepository.findById(assigmentId);
         if(optional.isPresent()){
             Assignment assignment = optional.get();
